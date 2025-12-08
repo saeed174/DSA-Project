b42fd0e8d68a0bd146daaaf1dc5b25d99245c026
Saeed Nabawy <saeednabawy28@gmail.com>
2025-12-08 18:13:41 +0200

    add new function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include "xmlCheck.h"
 #include <vector>
+#include <iostream>
+#include <cstddef>
 using namespace std;
 
 
@@ -10,16 +12,27 @@ int main(int argc, char* argv[]) {
     string inputFile = "sample.xml";
     string xmlContent = readXMLFile_withSpaces(inputFile);
 
-    vector<size_t> mismatchedPositions = findMismatchedTags(xmlContent);
+    vector<pair<size_t, string>> mismatchedPositions = findMismatchedTags(xmlContent);
     if (mismatchedPositions.empty()) {
         cout << "No mismatched tags found." << endl;
     } else {
-        cout << "Mismatched tags found at positions: ";
-        for (int pos : mismatchedPositions) {
-            cout << pos << " ";
+        for (int i = 0 ; i < mismatchedPositions.size() ; i++) {
+        
+            if (mismatchedPositions[i].second == "mismatched tag") {
+                cout << "There are mismatched tag at : " << mismatchedPositions[i].first << " ";
+                pair<size_t, string> nextIt = mismatchedPositions[i+1];
+                cout << nextIt.first << "\n";
+                ++i;
+            }
+            else{
+                cout << ("There are "+ mismatchedPositions[i].second + " at : ") << mismatchedPositions[i].first << " ";
+            }
         }
         cout << endl;
     }
+
+    string correctMismatched = correctMismatchedTags(xmlContent);
+    cout << correctMismatched;
 
     return 0;
 }
--- xmlCheck.h
+++ xmlCheck.h
@@ -6,7 +6,9 @@
 #include <sstream>
 #include "Stack.h"
 #include <vector>
+#include <map>
 #include <cctype>
+#include <algorithm>
 using namespace std;
 
 string readXMLFile_withoutSpaces(string filetag)
@@ -96,8 +98,8 @@ bool checkXMLConsistency(string xmlContent)
 }
 
 // errors can be occur: unclosed tags, unopened tags, mismatched tags, etc.
-vector<size_t> findMismatchedTags(const string &xml) {
-    vector<size_t> mismatchedPositions;
+vector<pair<size_t, string>> findMismatchedTags(const string &xml) {
+    vector<pair<size_t, string>> mismatchedPositions;
     vector<string> tagStack;
     vector<size_t> positionStack;
     size_t pos = 0;
@@ -124,7 +126,7 @@ vector<size_t> findMismatchedTags(const string &xml) {
 
             size_t closepos = xml.find('>', pos);
             if (closepos == string::npos) {
-                mismatchedPositions.push_back(pos); // unclosed tag start
+                mismatchedPositions.push_back(make_pair(pos,"unclosed tag")); // unclosed tag start
                 break;
             }
 
@@ -154,11 +156,14 @@ vector<size_t> findMismatchedTags(const string &xml) {
                 if (tagStack.empty() || tagStack.back() != name) {
                     // mismatch: report opening (if exists) and this closing
                     if (!positionStack.empty()) {
-                        mismatchedPositions.push_back(positionStack.back());
+                        mismatchedPositions.push_back(make_pair(positionStack.back() , "mismatched tag")); // '<' of opening
                         positionStack.pop_back();
                         tagStack.pop_back();
+                        mismatchedPositions.push_back(make_pair(pos, "mismatched tag"));       // '<' of closing
                     }
-                    mismatchedPositions.push_back(pos);       // '<' of closing
+                    else{
+                        mismatchedPositions.push_back(make_pair(pos, "unopened tag")); // unopened closing tag
+                    }
                 } else {
                     tagStack.pop_back();
                     positionStack.pop_back();
@@ -172,10 +177,78 @@ vector<size_t> findMismatchedTags(const string &xml) {
 
     // remaining unclosed openings
     while (!positionStack.empty()) {
-        mismatchedPositions.push_back(positionStack.back());
+        mismatchedPositions.push_back(make_pair(positionStack.back(), "unclosed tag"));
         positionStack.pop_back();
     }
 
     return mismatchedPositions;
 }
 #endif // XMLCHECK_H
+
+string correctMismatchedTags(string xml) {
+    string correct = xml;
+    vector<string> tagStack;
+    size_t pos = 0;
+
+    while (pos < correct.size()) {
+        if (correct[pos] != '<') {
+            ++pos;
+            continue;
+        }
+
+        size_t closepos = correct.find('>', pos);
+        if (closepos == string::npos) break;
+
+        string raw = correct.substr(pos + 1, closepos - pos - 1);
+        if (raw.empty()) {
+            pos = closepos + 1;
+            continue;
+        }
+
+        bool isClosing = (raw[0] == '/');
+        size_t lastNonSpace = raw.find_last_not_of(" \t\r\n");
+        bool selfClosing = (!isClosing &&
+                            lastNonSpace != string::npos &&
+                            raw[lastNonSpace] == '/');
+
+        if (!isClosing) {
+            string name = raw;
+            if (selfClosing) {
+                name = name.substr(0, lastNonSpace);
+            }
+            size_t spacePos = name.find(' ');
+            if (spacePos != string::npos)
+                name = name.substr(0, spacePos);
+
+            if (!name.empty() && !selfClosing) {
+                tagStack.push_back(name);
+            }
+
+        } else {
+            // Closing tag
+            string name = raw.substr(1);
+            size_t spacePos = name.find(' ');
+            if (spacePos != string::npos)
+                name = name.substr(0, spacePos);
+
+            if (tagStack.empty()) {
+                // No opening tag to match, leave it as-is
+            } else if (tagStack.back() != name) {
+                string openName = tagStack.back();
+                tagStack.pop_back();
+                size_t replaceStart = pos + 2;
+                size_t replaceLen   = name.length();
+
+                correct.replace(replaceStart, replaceLen, openName);
+
+            } else {
+                tagStack.pop_back();
+            }
+        }
+
+        closepos = correct.find('>', pos);
+        if (closepos == string::npos) break;
+        pos = closepos + 1;
+    }
+    return correct;
+}
